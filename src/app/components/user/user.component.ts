23d53218480b05f871b04f42856681c03ee8a1ec
Trekh <danieltamriv@gmail.com>
2025-03-09 12:15:23 -0500

    Agregación de corazón en el apartado de usuario

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,43 +1,41 @@
-import { Component,Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { heart, heartOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
-  imports: [CommonModule, IonicModule],
+  standalone: true, // Si usas Angular Standalone Components
+  imports: [CommonModule, IonicModule]
 })
-export class UserComponent  implements OnInit {
-
+export class UserComponent implements OnInit {
   @Input() character: any;
 
-  constructor() { }
+  constructor() {
+    // Registrar los iconos manualmente
+    addIcons({
+      'heart': heart,
+      'heart-outline': heartOutline
+    });
+  }
 
   ngOnInit() {}
 
-
-  toggleFavorito(unPersonaje:any){
-    console.log("ADDFavorite",unPersonaje);
-    //this.storageService.saveRemovePersonaje(unPersonaje);
- 
- 
+  toggleFavorito(unPersonaje: any) {
+    console.log("ADDFavorite", unPersonaje);
+    
+    // Simular que cambia el estado con una clase animada
+    const icon = document.querySelector('.favorite-icon');
+    if (icon) {
+      icon.classList.add('active');
+    }
   }
- 
- 
-  esFavorito(unPersonaje:any):boolean{
- 
- 
-    /*
-    if(this.storageService.personajeInFavorites(unPersonaje)){
-      return true;
-    }
-    else {
-      return false;
-    }
-    */
+  
+
+  esFavorito(unPersonaje: any): boolean {
     return false;
   }
- 
-
 }
