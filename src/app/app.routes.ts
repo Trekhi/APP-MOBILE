1664fbf5855ac4d425666106c0cae0e6a6148daa
Trekh <danieltamriv@gmail.com>
2025-03-23 19:12:32 -0500

    Creación de nuevos estilos, headaer y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,14 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },  {
-    path: 'personaje-list',
-    loadComponent: () => import('./components/personaje-list/personaje-list.page').then( m => m.PersonajeListPage)
+  },
+  {
+    path: 'header',
+    loadComponent: () => import('./components/header/header.component').then( m => m.HeaderComponent)
+  },
+  {
+    path: 'footer',
+    loadComponent: () => import('./components/footer/footer.component').then( m => m.FooterComponent)
   },
 
 ];
