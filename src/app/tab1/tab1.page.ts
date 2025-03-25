c97c8d8270d95eb363f1a2aff11ed0cbdcf9230e
Trekh <danieltamriv@gmail.com>
2025-03-25 18:13:53 -0500

    Libreria Swiper

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent,IonImg } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 //import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonImg],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, HeaderComponent, FooterComponent],
 })
 export class Tab1Page {
   constructor() {}
